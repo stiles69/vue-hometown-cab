f428b00832d339213242177ee99bca646bed306f
stiles69 <brett.salemink@gmail.com>
2018-04-25 22:02:32 -0500

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,46 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  VCard,
+  VDivider,
+  transitions
+} from 'vuetify'
+import '../node_modules/vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    VCard,
+    VDivider,
+    transitions
+  },
+  theme: {
+    primary: '#FDD835',
+    secondary: '#FFF59D',
+    accent: '#FFD600',
+    error: '#f44336',
+    warning: '#FF5722',
+    info: '#2196f3',
+    success: '#4caf50'
+  },
+})
 
 Vue.config.productionTip = false
 
